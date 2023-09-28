95d2c84830ddf4f8197b2ca64ba5804d631042ec
CEPHAL0 <xaradxarma@gmail.com>
2023-09-28 22:28:54 +0545

    Updated Backend

--- api/main.py
+++ api/main.py
@@ -6,7 +6,8 @@ from PIL import Image
 import tensorflow as tf
 
 MODEL = tf.keras.models.load_model("../deep_learning/models/model_1")
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+MODEL2 = tf.keras.models.load_model("../deep_learning/models/model_2")
+CLASS_NAMES = ["early_blight", "late_blight", "healthy"]
 
 # SCRIPT: uvicorn main:app --host localhost --port 8000 --reload
 
@@ -35,7 +36,28 @@ async def predict(file: UploadFile = File()):
     img_batch = np.expand_dims(image, 0)
     predictions = MODEL.predict(img_batch)
 
-    return f"{predictions}"
+    return f"Predictions using Model 1: {predictions}"
+
+
+@app.post("/predict2")
+async def predict(file: UploadFile = File()):
+
+    image = read_file_as_image(await file.read())
+    # bytes = await file.read()
+
+    # The model accepts the data in format [[]] format but we have the image in [] format, so we have to expand the dims in the numpy array of the image
+
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL2.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions)]
+    confidence = np.max(predictions[0])
+
+    return {
+        "class": predicted_class,
+        "confidence": str(confidence)
+    }
+
+    # return f"Predictions using Model 2: {predictions} \n Predicted Class = {predicted_class}, confidence: {confidence}"
 
 if __name__ == "__main__":
     uvicorn.run(app=app, host='localhost', port=8000, reload=True)
